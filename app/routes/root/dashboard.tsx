07260e8a9e65612818f27b86789a261712dce53e
Arief-alt <145629626+Arief-alt@users.noreply.github.com>
2025-07-03 21:58:27 +0700

    Add Title

--- app/routes/root/dashboard.tsx
+++ app/routes/root/dashboard.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <main className="pt-20 wrapper pb-20">
+            <header className="flex flex-col justify-center items-center gap-6">
+                <h1 className="text-5xl font-semibold">
+                    E-CommerceShowcase
+                </h1>
+
+                <span className="text-2xl">
+                    Discover premium products with exceptional quality and unbeatable prices
+                </span>
+            </header>
+        </main>
     )
 }
 export default Dashboard
